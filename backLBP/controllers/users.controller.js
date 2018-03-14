0676a1ba959c3a607f4557116233b507f4fb8bf1
AliciaJz <zilaphono@gmail.com>
2018-03-14 13:17:47 -0600

    controllers

---
+++ backLBP/controllers/users.controller.js
@@ -0,0 +1,18 @@
+const User = require ('../models/User');
+
+exports.getUsers = function(req, res, next) {
+    User.find()
+    .then(users => res.status(200).json(users))
+    .catch(e => res.status(500).send(e));
+  }
+
+  exports.postUsers = (req, res, next) =>{
+    const newUser = new User({
+      username: req.body.username,
+      email: req.body.email
+    });
+
+    newUser.save()
+    .then(user => res.tatus(201).json(user))
+    .catch(e => res.status(500).send(e))
+  }
--- backLBP/routes/users.js
+++ backLBP/routes/users.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
+// const User = require ('../models/User');
+
+const controller = require ('../controllers/users.controller');
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get('/', controller.getUsers);
+router.post('/', controller.postUsers);
 
 module.exports = router;
